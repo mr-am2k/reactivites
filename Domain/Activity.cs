4f43877007dc371c9c00a42f1919a65406406b46
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-07-30 16:57:07 +0200

    Adding changes from all folders, not only API, this includes now all changes mentiond in the message before

---
+++ Domain/Activity.cs
@@ -0,0 +1,13 @@
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; } //GUID - global unique identifier, if it's called Id, entity framework will know that this should be Id for the entity
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
