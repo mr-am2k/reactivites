978c76e092ec95f4154378621b6f9ef05f172330
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-07-30 16:55:09 +0200

    End of module 1, created skeleton API, added migrations for our first data model and created reactivites database, also added some data to the database using Seed class

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//Next 10 lines of code are used to create or update database based on migration
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}catch (Exception ex){
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
