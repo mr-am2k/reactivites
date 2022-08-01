cc42d2e7f9ff7234c3b5df198534d8f99cf1e81d
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-01 23:25:54 +0200

    Created fetch request for fetching activites from the backend using axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  const fetchActivities = async () => {
+    const response = await axios.get('http://localhost:5000/api/activities');
+    setActivities(response.data);
+  };
+
+  useEffect(() => {
+    fetchActivities();
+  }, []);
+
   return (
-    <div className="App">
-     Reactivities
+    <div>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
     </div>
   );
 }
