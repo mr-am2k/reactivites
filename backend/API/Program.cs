53518d24cf1cd7bc573a829079188f86d120b4e0
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-01 23:26:40 +0200

    Solved problem with CORS policy

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -5,26 +5,41 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//allows us to send requests to the backend
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        //AnyMethod means that allows all methods (GET,POST,PUT,...), AnyHeader means that allow all headers and AnyOrigin means that allows all origins, (domain is't important)
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//Next 10 lines of code are used to create or update database based on migration, this can be done in terminal (dotnet ef database update command)
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
+//Next scope is used to create or update database based on migration, this can be done in terminal (dotnet ef database update command)
 {
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
-}catch (Exception ex){
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred during migration");
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+        await Seed.SeedData(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred during migration");
+    }
 }
 
 // Configure the HTTP request pipeline.
@@ -35,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
