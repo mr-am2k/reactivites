815342af736953cb6d0f81471bc06b08c521ed02
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-02 12:16:44 +0200

    Updated gitignore file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Header } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
