7d5a51a2beb95fa790473abd4257fce49cbb6662
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-08-23 13:36:16 +0200

    Added design for ActivityDetailChat component

--- frontend/src/components/Activities/ActivityDetails/ActivityDetailChat/ActivityDetailChat.tsx
+++ frontend/src/components/Activities/ActivityDetails/ActivityDetailChat/ActivityDetailChat.tsx
@@ -1,9 +1,49 @@
-import classes from './ActivityDetailChat.module.css'
+import {BiCommentCheck} from 'react-icons/bi'
+import classes from './ActivityDetailChat.module.css';
+import user from '../../../../assets/user.png';
+
+const comments = [
+  {
+    image: user,
+    name: 'Matt',
+    time: 'Today at 5:42PM',
+    content: 'How artistic!',
+  },
+  {
+    image: user,
+    name: 'Joe Henderson',
+    time: '5 days ago',
+    content: 'Dude, this is awesome. Thanks so much',
+  },
+];
 
 const ActivityDetailChat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className={classes.activityDetailChatContainer}>
+      <div className={classes.activityDetailInfoHeader}>
+        <h3>Chat about this event</h3>
+      </div>
+      <div className={classes.activityDetailInfoMessages}>
+        {comments.map((comment,index) => (
+          <div key={index} className={classes.userMessage}>
+            <img src={comment.image} alt='user' />
+            <div className={classes.userMessageInfo}>
+              <h3>
+                {comment.name}
+                <span className={classes.dateInfo}>{comment.time}</span>
+              </h3>
+              <p>{comment.content}</p>
+              <button>Replay</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={classes.activityDetailInfoMessageBox}>
+        <textarea className={classes.messageInput}/>
+        <button><BiCommentCheck className={classes.icon}/> Add replay</button>
+      </div>
+    </div>
+  );
+};
 
-export default ActivityDetailChat
+export default ActivityDetailChat;
