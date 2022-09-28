d813c81a1919c9c98c98ab452179c4983f56d8ef
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-09-28 17:10:48 +0200

    Added Account Controller which will be responsible for user login/register

---
+++ backend/API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) //ActionResult takes as paramet, object that is supposed to return
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null) return Unauthorized("Wrong email"); //if there is no match for email, than variable user will be null which means that user can't login
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if(result.Succeeded)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = "This will be a token",
+                    Username = user.UserName
+                };
+            }
+            return Unauthorized();
+        }
+    }
+}
